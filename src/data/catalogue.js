3255d9844b03de4d66133a23b4ef338ebbfd9529
Asila Stephen <stephen4asila@gmail.com>
2026-06-04 13:12:34 +0100

    refactor: relocate catalogue data, rename images

---
+++ src/data/catalogue.js
@@ -0,0 +1,49 @@
+export const shopItems = [{
+        src: "/assets/images/Brand Demo/BLACK_LEAGUE_POLO_FRONT.jpg",
+        name: "League Polo",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_belt_5f837ca2-5f59-429c-9d03-e48d757672d3.jpg",
+        name: "Bola Belt",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_black_skullcap_ecommerce_front_flat.jpg",
+        name: "Black Skullcap",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_burgundy_skullcap_ecommerce_front_flat.jpg",
+        name: "Burgundy Skullcap",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_coffee_skullcap_ecommerce_front_flat.jpg",
+        name: "Coffee Skullcap",
+    },
+    {
+        src: "/assets/images/Brand Demo/BOLAPSD_Emberline_2-Piece_Set_FRONT.jpg",
+        name: "Emberline 2-piece",
+    },
+    {
+        src: "/assets/images/Brand Demo/BOLAPSD_EMBERLINE_DRESS.jpg",
+        name: "Emberline Dress",
+    },
+    {
+        src: "/assets/images/Brand Demo/BOLAPSD_Emberline_Romper_FRONT.jpg",
+        name: "Emberline Romper",
+    },
+    {
+        src: "/assets/images/Brand Demo/BOLAPSD_Emberline_Romper_RED_FRONT.jpg",
+        name: "Emberline Romper Red",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_grey_skullcap_ecommerce_front_flat.jpg",
+        name: "Grey Skullcap",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_polo_ecommerce_blue_flat.jpg",
+        name: "Blue Polo",
+    },
+    {
+        src: "/assets/images/Brand Demo/bolapsd_red_skullcap_ecommerce_front_flat.jpg",
+        name: "Red Skullcap",
+    },
+];
